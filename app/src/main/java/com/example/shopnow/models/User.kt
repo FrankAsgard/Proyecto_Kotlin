8ad36dc638da708ed30d34bed8c74968a82d0d9c
Juan-Camilo-Sanchez-Echeverri <juancamilosanche65@gmail.com>
2024-06-03 12:15:18 -0500

    refactor: Add ProgressDialogFragment for displaying progress during async tasks

--- app/src/main/java/com/example/shopnow/models/User.kt
+++ app/src/main/java/com/example/shopnow/models/User.kt
@@ -1,8 +1,37 @@
 package com.example.shopnow.models
 
+import com.google.firebase.firestore.PropertyName
+
+enum class Role(val role: String) {
+    SUPER_ADMIN("super_admin"),
+    CLIENT("client");
+}
+
+
 data class User(
-    val username: String,
-    val password: String,
-    val email: String,
-    val fullName: String
-)
+    @get:PropertyName("userId")
+    @set:PropertyName("userId")
+    var userId: String? = null,
+
+    @get:PropertyName("username")
+    @set:PropertyName("username")
+    var username: String? = null,
+
+    @get:PropertyName("password")
+    @set:PropertyName("password")
+    var password: String? = null,
+
+    @get:PropertyName("email")
+    @set:PropertyName("email")
+    var email: String? = null,
+
+    @get:PropertyName("fullName")
+    @set:PropertyName("fullName")
+    var fullName: String? = null,
+
+    @get:PropertyName("role")
+    @set:PropertyName("role")
+    var role: Role? = null
+) {
+    constructor() : this(null, null, null, null, null, null)
+}
